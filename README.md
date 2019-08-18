9d15b85e01c91ab056e8e9ef4283e851a579e7a0
Carlos Vasquez <carlos@cobranix.com>
2019-08-17 22:00:09 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Raspbery Pi code examples
+
+Documenting various code examples for the Raspbery Pi
+
+- Blinking an LED
+- Controlling a Servo motor
+- Basic Gobot usage
+- Easily adding an API with Gobot
+- Displaying info to OLED
---
+++ python/blink.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+from gpiozero import LED
+from time import sleep
+
+# Use correct GPIO pin number
+led = LED(17) 
+
+# Blink LED every second until program exits
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
